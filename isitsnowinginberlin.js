17d0690cd6c0ee187d0fc67b325e106cb7027249
Jeremy Mayeres <jerr0328@gmail.com>
2014-12-07 01:32:48 +0100

    Changed tabs to spaces, implemented redis for caching, removed API key from code (using environment variable now), added some JSON API calls.

--- isitsnowinginberlin.js
+++ isitsnowinginberlin.js
@@ -1,58 +1,101 @@
 var request = require('request');
 var restify = require('restify');
-
-var lastUpdateTime = new Date(0);
-var isSnowing = false;
-var lastWx = 0;
+var redis = require('redis');
+var url = require('url');
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+client.auth(redisURL.auth.split(":")[1]);
+// Update delay in seconds
+var updateDelaySeconds = 60;
 
 // Berlin city code for weather is 2950159
 var weatherApiOptions = {
-	url: "http://api.openweathermap.org/data/2.5/weather?id=2950159",
-	headers: {"x-api-key": "39a5b0283a8385e239fe8ce89d5a9b76"}
+  url: "http://api.openweathermap.org/data/2.5/weather?id=2950159",
+  headers: {"x-api-key": process.env.OWMAPIKEY}
 };
 
-function needsUpdating() {
-	return (new Date() - lastUpdateTime) > 1000
-};
-
-function updateWeather() {
-	request(weatherApiOptions, function (error, response, body){
-		if(!error && response.statusCode == 200){
-			lastWx = JSON.parse(body);
-			isSnowing = "Snow" == lastWx.weather[0].main;
-			lastUpdateTime = new Date();
-			console.log("Got weather " + lastWx.weather[0].main + " at " + lastUpdateTime.toString());
-		}
-		else {
-			console.error(error);
-		}
-	});
-};
-
-function respond(req, res, next) {
-	if(needsUpdating()) {
-		updateWeather();
-	}
-	var body = "<html><head><title>Is it snowing in Berlin?</title><body><h1>";
-	if(isSnowing)
-		body += "Yes"
-	else
-		body += "No"
-	body += "</h1></body></html>"
-	res.writeHead(200, {
-		'Content-Length': Buffer.byteLength(body),
-		'Content-Type': 'text/html'
-	});
-	res.write(body);
-	res.end();
-	next();
+function saveToRedis(wx) {
+  // Set the weather (expires automatically)
+  client.set("cached-wx", wx);
+  client.expire("cached-wx", updateDelaySeconds);
+  // Keep it in non-expiring key (for debugging)
+  client.set("last-wx", wx);
 }
 
-updateWeather();
+function getWeather(callback) {
+  client.get("cached-wx", function (err, reply) {
+    if(reply) {
+      wx = JSON.parse(reply);
+      callback(wx);
+      console.log("Used cache");
+    }
+    else {
+      updateWeather(saveToRedis, callback);
+      console.log("Needed to update cache");
+    }
+  });
+}
+
+function updateWeather(storeWx, callback) {
+  request(weatherApiOptions, function (error, response, body){
+    if(!error && response.statusCode == 200){
+      wx = JSON.parse(body);
+      storeWx(body);
+      callback(wx);
+    }
+    else {
+      console.error(error);
+    }
+  });
+};
+
+function isSnowing(wx) {
+  return "Snow" == wx.weather[0].main;
+}
+
+function APIisSnowing(req, res, next) {
+  getWeather(function (wx) {
+    if(isSnowing(wx))
+      res.send({isSnowing: true});
+    else
+      res.send({isSnowing: false});
+  });
+}
+
+function APIgetRawWeather(req, res, next) {
+  getWeather(function (wx){
+    res.send(wx);
+  });
+}
+
+function respond(req, res, next) {
+  getWeather(function (wx) {
+    var body = "<html><head><title>Is it snowing in Berlin?</title><body><h1 style=\"text-align: center\">";
+    if(isSnowing(wx))
+      body += "Yes"
+    else
+      body += "No"
+    body += "</h1><p>Data last updated: " + new Date(wx.dt).toString() + "</p></body></html>"
+    res.writeHead(200, {
+      'Content-Length': Buffer.byteLength(body),
+      'Content-Type': 'text/html'
+    });
+    res.write(body);
+    res.end();
+  })
+
+  next();
+}
+console.log("Getting initial value: ");
+getWeather(console.log);
 
 var server = restify.createServer();
 server.get("/", respond);
 server.head("/", respond);
+server.get("/api/isSnowing", APIisSnowing);
+server.head("/api/isSnowing", APIisSnowing);
+server.get("/api/rawWeather", APIgetRawWeather);
+server.head("/api/rawWeather", APIgetRawWeather);
 
 server.listen(process.env.PORT || 5000, function() {
   console.log('%s listening at %s', server.name, server.url);
