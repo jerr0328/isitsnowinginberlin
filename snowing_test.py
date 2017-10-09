b679ad53cc5ec6dc064544a9496dae525423a900
Sey Cheon <22985121+Sey1000@users.noreply.github.com>
2017-10-09 15:27:19 +0200

    Add superbasic truthiness test

---
+++ snowing_test.py
@@ -0,0 +1,12 @@
+import unittest
+# import os
+# import flask
+# from isitsnowinginberlin import isitsnowinginberlin
+
+class SnowingTests(unittest.TestCase):
+    def test_truthiness(self):
+        five = 5
+        self.assertEqual(five, 5)
+
+if __name__ == "__main__":
+    unittest.main()
